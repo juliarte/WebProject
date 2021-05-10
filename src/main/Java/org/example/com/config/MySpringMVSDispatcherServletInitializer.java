c923b7973251f9765f15be9a8843a3d0b9b6495f
Julia Artemieva <jul.art1302@gmail.com>
2021-05-10 10:02:37 +0300

    add class instead of web.xml

---
+++ src/main/Java/org/example/com/config/MySpringMVSDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package org.example.com.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVSDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
